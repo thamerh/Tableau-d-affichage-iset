1d4212087ebba04e1b8d2f2f8eeda9ab333c7e09
thamerh <hamdithamer99@gmail.com>
2022-04-21 14:40:34 +0200

    feat: add all Table structure

--- serveur/auth_db.sql
+++ serveur/auth_db.sql
@@ -11,37 +11,118 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `carte_etudiant`
+-- Table structure for table `departement`
 --
 
-CREATE TABLE carte_etudiant(
-   num_insc INT,
+CREATE TABLE departement(
+   nom_dep VARCHAR(50),
+   PRIMARY KEY(nom_dep)
+);
+--
+-- Table structure for table `donnes_dotorisation` for chef department register
+--
+CREATE TABLE donnes_dotorisation(
    cin VARCHAR(50),
-   classe VARCHAR(50),
-   PRIMARY KEY(num_insc, cin)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
--- --------------------------------------------------------
-
+   code_dautorisation VARCHAR(50),
+   PRIMARY KEY(cin, code_dautorisation)
+);
 --
--- Table structure for table `etudiants`
+-- Table structure for table `classe`
 --
-CREATE TABLE `etudiants`(
+CREATE TABLE classe(
+   lib_class VARCHAR(50),
+   nom_dep VARCHAR(50) NOT NULL,
+   PRIMARY KEY(lib_class),
+   FOREIGN KEY(nom_dep) REFERENCES departement(nom_dep)
+);
+--
+-- Table structure for table `chef_departement`
+--
+CREATE TABLE chef_departement(
    id VARCHAR(50),
    name VARCHAR(50),
    email VARCHAR(50),
-   num_insc INT NOT NULL,
-   cin VARCHAR(50) NOT NULL,
    password VARCHAR(255),
    refresh_token VARCHAR(255),
    createdAt DATETIME NOT NULL,
    updatedAt DATETIME NOT NULL,
+   cin VARCHAR(50) NOT NULL,
+   code_dautorisation VARCHAR(50) NOT NULL,
+   nom_dep VARCHAR(50) NOT NULL,
    PRIMARY KEY(id),
-   FOREIGN KEY(num_insc, cin) REFERENCES carte_etudiant(num_insc, cin)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+   FOREIGN KEY(cin, code_dautorisation) REFERENCES donnes_dotorisation(cin, code_dautorisation),
+   FOREIGN KEY(nom_dep) REFERENCES departement(nom_dep)
+);
+--
+-- Table structure for table `notification`
+--
+CREATE TABLE notification(
+   id VARCHAR(50),
+   sujet VARCHAR(255),
+   createdAt DATETIME NOT NULL,
+   lib_class VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(lib_class) REFERENCES classe(lib_class)
+);
+--
+-- Table structure for table `message` inheritance in table `notification`
+--
+CREATE TABLE message(
+   id VARCHAR(50),
+   msg TEXT,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id) REFERENCES notification(id)
+);
+--
+-- Table structure for table `image` inheritance in table `notification`
+--
+CREATE TABLE image(
+   id VARCHAR(50),
+   nom_img VARCHAR(250),
+   type_img VARCHAR(250),
+   demention_h VARCHAR(50),
+   dimention_v VARCHAR(50),
+   image blob,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id) REFERENCES notification(id)
+);
+--
+-- Table structure for table `carte_etudiant`
+--
+CREATE TABLE carte_etudiant(
+   cin VARCHAR(50),
+   num_insc VARCHAR(50),
+   lib_class VARCHAR(50) NOT NULL,
+   PRIMARY KEY(cin, num_insc),
+   FOREIGN KEY(lib_class) REFERENCES classe(lib_class)
+);
+--
+-- Table structure for table `etudiants`
+--
+
+CREATE TABLE etudiants(
+   id VARCHAR(50),
+   name VARCHAR(50),
+   email VARCHAR(50),
+   password VARCHAR(255),
+   refresh_token VARCHAR(255),
+   createdAt DATETIME NOT NULL,
+   updatedAt DATETIME NOT NULL,
+   cin VARCHAR(50) NOT NULL,
+   num_insc VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(cin, num_insc) REFERENCES carte_etudiant(cin, num_insc)
+);
+
+--
 -- AUTO_INCREMENT for table `etudiants`
 --
 ALTER TABLE `etudiants`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+--
+-- AUTO_INCREMENT for table `chef_departement`
+--
+ALTER TABLE `chef_departement`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
