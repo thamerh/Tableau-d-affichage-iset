b00b4af8d2370fcc45cb44417aaff507379aad8d
thamerh <hamdithamer99.com>
2022-05-19 16:58:28 +0000

    feat: create feature for add chef data

---
+++ client/src/screens/AddCodeEtu.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useHistory,Link } from 'react-router-dom';
+
+export  const AddEtuCard = () => {
+    const [cin, setCin] = useState('');
+    const [num_insc, setNum_insc] = useState('');
+    const [lib_class, setLib_class] = useState('');
+    const [msg, setMsg] = useState('');
+    const history = useHistory();
+
+    const Auth = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/AddCarteEtu', {
+                cin:cin, 
+                num_insc: num_insc,
+                lib_class: lib_class
+            });
+            alert("add successfully")
+            history.push("/dashboardAdmin");
+        } catch (error) {
+            if (error.response) {
+                alert(error.response.data.msg);
+            }
+        }
+    }
+
+    return (
+        <div style={ HeaderStyle }>
+        <section className=" is-fullheight is-fullwidth" >
+            <div className="hero-body">
+                <div className="container">
+                    <div className="columns is-centered">
+                        <div className="column is-4-desktop">
+                            <form onSubmit={Auth} className="box">
+                                <h1 className="has-text-centered google-font">Add Student Card Data</h1>
+                                <div className="field mt-5">
+                                    <label className="label">CIN: </label>
+                                    <div className="controls">
+                                        <input type="text" className="input is-rounded" placeholder="CIN" value={cin} onChange={(e) => setCin(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Numero d'inscrit:</label>
+                                    <div className="controls">
+                                        <input type="text" className="input is-rounded" placeholder="Numero d'inscrit" value={num_insc} onChange={(e) => setNum_insc(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Class:</label>
+                                    <div className="controls">
+                                        <input type="text" className="input is-rounded" placeholder="Class" value={lib_class} onChange={(e) => setLib_class(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <button className="button is-info is-rounded is-fullwidth">Add</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </div>
+        
+    )
+}
+
+export const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url("https://images.pexels.com/photos/633409/pexels-photo-633409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+}
