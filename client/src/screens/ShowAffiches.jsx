ac353ecdc5fadd3bde4e933722b42635228e4072
thamerh <hamdithamer99.com>
2022-05-21 10:48:26 +0000

    chore: delete verify token feature in showAffiche component

--- client/src/screens/ShowAffiches.jsx
+++ client/src/screens/ShowAffiches.jsx
@@ -7,49 +7,7 @@ import { useHistory } from 'react-router-dom';
 import './secreens.css';
 
 const ShowAffiche = () => {
-    const [name, setName] = useState('');
-    const [token, setToken] = useState('');
-    const [expire, setExpire] = useState('');
-    const [users, setUsers] = useState([]);
     const history = useHistory();
-
-    useEffect(() => {
-        refreshToken();
-        // getUsers();
-    }, []);
-
-    const refreshToken = async () => {
-        try {
-            const response = await axios.get('http://localhost:5000/tokenAdmin');
-            setToken(response.data.accessToken);
-            const decoded = jwt_decode(response.data.accessToken);
-            setName(decoded.name);
-            setExpire(decoded.exp);
-        } catch (error) {
-            if (error.response) {
-                history.push("/loginAdmin");
-            }
-        }
-    }
-
-    const axiosJWT = axios.create();
-
-    axiosJWT.interceptors.request.use(async (config) => {
-        const currentDate = new Date();
-        if (expire * 1000 < currentDate.getTime()) {
-            const response = await axios.get('http://localhost:5000/tokenAdmin');
-            config.headers.Authorization = `Bearer ${response.data.accessToken}`;
-            setToken(response.data.accessToken);
-            const decoded = jwt_decode(response.data.accessToken);
-            setName(decoded.name);
-            setExpire(decoded.exp);
-        }
-        return config;
-    }, (error) => {
-        return Promise.reject(error);
-    });
-
-
     const [Affiche, setAffiche] = useState([])
 
     useEffect(() => {
@@ -63,12 +21,12 @@ const ShowAffiche = () => {
     },[])
 
     return (
-        <div style={HeaderStyle}>
+        <div >
            <Container  className="justify-content-center p-2">
-               <div className='   border-bottom border-white'>
+               {/* <div className='   border-bottom border-white'>
                <h1 className='text-center text-white FontFamily'>Show All Affiche</h1>
                <a href='dashboardAdmin'><img src='https://static.thenounproject.com/png/2739572-200.png' alt='tttt' className='ImgIconAdmin'/></a>
-               </div>
+               </div> */}
 
                <Row >
                     {
@@ -89,12 +47,3 @@ const ShowAffiche = () => {
 }
 
 export default ShowAffiche
-const HeaderStyle = {
-    width: "100%",
-    minHeight: "757px",
-    height: "100%",
-    background: `url("https://images.pexels.com/photos/633409/pexels-photo-633409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
-}
