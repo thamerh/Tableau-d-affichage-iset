5be2c2f84652b2cc463a6e2de46c936941e6868e
thamerh <hamdithamer99@gmail.com>
2022-06-04 00:40:42 +0200

    feat: add screen for update affiche chef department

---
+++ client/src/screens/Chef-dep/EditAffiche.jsx
@@ -0,0 +1,141 @@
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
+import { Button, Container, Form} from 'react-bootstrap'
+import {  useParams } from 'react-router'
+import jwt_decode from "jwt-decode";
+import { useHistory } from 'react-router-dom';
+const EditAffiche = () => {
+
+    const [name, setName] = useState('');
+    const [token, setToken] = useState('');
+    const [expire, setExpire] = useState('');
+    const [users, setUsers] = useState([]);
+    const history = useHistory();
+
+    useEffect(() => {
+        refreshToken();
+        // getUsers();
+    }, []);
+
+    const refreshToken = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/tokenChef');
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setName(decoded.name);
+            setExpire(decoded.exp);
+            
+        } catch (error) {
+            if (error.response) {
+                history.push("/LoginChef");
+            }
+        }
+       
+    }
+    
+    const axiosJWT = axios.create();
+
+    axiosJWT.interceptors.request.use(async (config) => {
+        const currentDate = new Date();
+        if (expire * 1000 < currentDate.getTime()) {
+            const response = await axios.get('http://localhost:5000/tokenChef');
+            config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            setToken(response.data.accessToken);
+            const decoded = jwt_decode(response.data.accessToken);
+            setName(decoded.name);
+            setExpire(decoded.exp);
+           
+        }
+        return config;
+    }, (error) => {
+        return Promise.reject(error);
+    });
+    const { id } = useParams()
+   
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [published, setPublished] = useState(true)
+
+    useEffect(() => {
+        const getDataById = async () => {
+            const {data} = await axios.get(`http://localhost:5000/AfficheChef/${id}`)
+            setTitle(data.title)
+            setDescription(data.description)
+            setPublished(data.published)
+        }
+
+        getDataById()
+    },[id])
+
+   const updateHandler = async (e) => {
+
+        e.preventDefault()
+       
+        // update by put request
+
+        const data = {
+            title: title,
+            description: description,
+            published: published
+        }
+
+        await axios.put(`http://localhost:5000/AfficheChef/edit/${id}`, data)
+
+        history.push('/Afficheschef')
+
+   }
+
+    return (
+        <div style={HeaderStyle} className='row'>
+         
+            <Container className='mt-5 p-2 '  >
+            <div className='border-bottom border-white'>
+                <h1  className="text-center text-white FontFamily">Edit Affiche</h1>
+                <a href='/Afficheschef'><img src='https://static.thenounproject.com/png/2739572-200.png' alt='tttt' className='ImgIconAdmin'/></a>
+             </div>
+                <Form onSubmit={updateHandler} className='w-100 p-3 col-md-6'>
+                    <Form.Group className="mb-3" controlId="title">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            type="text"
+                          />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            as="textarea"
+                            />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="publishedCheckedid">
+                        <Form.Check
+                            type="checkbox"
+                            value={published}
+                            onChange={(e) => setPublished(e.target.checked)}
+                            label="publish"
+                           />
+                    </Form.Group>
+
+
+                    <Button  type="submit">
+                        Update 
+                    </Button>
+                </Form>
+            </Container>
+        </div>
+    )
+}
+
+export default EditAffiche
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url("https://images.pexels.com/photos/633409/pexels-photo-633409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+}
