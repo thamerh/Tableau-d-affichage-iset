2458418f563437bca97ef3dabbf53c287e80a9f0
thamerh <hamdithamer99@gmail.com>
2022-07-30 22:33:32 +0100

    feat: tttt

--- client/src/components/Chef-Department/Dashboard.js
+++ client/src/components/Chef-Department/Dashboard.js
@@ -1,19 +1,30 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import jwt_decode from "jwt-decode";
 import { useHistory } from 'react-router-dom';
+import {
+  CDBBtn,
+  CDBProgress,
+  CDBTable,
+  CDBTableHeader,
+  CDBTableBody,
+  CDBContainer,
+  CDBLink } from "cdbreact";
+import { Pie, Bar } from "react-chartjs-2";
+import Sidebar from "./SideBar";
+import Navbar from "./NavBar";
+import '../Admin/Dashboard.css'
+import ShowEmploi  from '../../screens/Chef-dep/ShowEmplois'
+import ShowAffiche from '../../screens/Chef-dep/ShowAffiches'
 
-const DashboardChef = () => {
+export const  DashboardChef= () => {
     const [name, setName] = useState('');
     const [token, setToken] = useState('');
     const [expire, setExpire] = useState('');
-    const [users, setUsers] = useState([]);
     const history = useHistory();
 
     useEffect(() => {
         refreshToken();
-        // getUsers();
     }, []);
 
     const refreshToken = async () => {
@@ -47,21 +58,89 @@ const DashboardChef = () => {
         return Promise.reject(error);
     });
 
-    // const getUsers = async () => {
-    //     const response = await axiosJWT.get('http://localhost:5000/users', {
-    //         headers: {
-    //             Authorization: `Bearer ${token}`
-    //         }
-    //     });
-    //     setUsers(response.data);
-    // }
 
-    return (
-        <div className="container mt-5">
-            <h1 className="columns is-centered" >Welcome Back: {name}</h1>
-           
+  const data = {
+    chart1:{
+      labels: [
+        "Eating",
+        "Drinking",
+        "Sleeping",
+      ],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: [
+            "#F2C94C",
+            "#2F80ED",
+            "#9B51E0",
+          ],
+          borderWidth: 0,
+          data: [9, 22, 7],
+        },
+      ]
+    },
+    chart2:{
+      labels: [
+        "Eating",
+        "Drinking",
+        "Sleeping",
+        "Designing",
+        "Coding",
+        "Cycling",
+        "Running",
+      ],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgba(255, 153, 51, 0.8)",
+          borderColor: "rgb(102, 51, 0)",
+          data: [65, 59, 75, 81, 56, 55, 40],
+        },
+        {
+          label: "My Second dataset",
+          backgroundColor: "#2F80ED",
+          borderColor: "rgb(0, 41, 102)",
+          data: [38, 48, 60, 79, 96, 47, 80],
+        },
+      ]
+    }
+  }
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {display:false}, 
+    scales: {
+      xAxes: [{
+        ticks: {
+          display: false
+        }, 
+      }],
+      yAxes: [{
+        gridLines: {
+          display:false
+        },
+        ticks: {
+          display: false
+        }
+      }]
+    }
+  }
+
+  return ( 
+    <div className="dashboard d-flex">
+    	<div>
+      	<Sidebar/>
+      </div>
+      <div style={{flex:"1 1 auto", display:"flex", flexFlow:"column", height:"100vh", overflowY:"hidden"}}>
+        <Navbar/>
+        <div style={{height:"100%"}} className="overflow-auto">
+        <ShowEmploi/>
+      
+        <ShowAffiche/>
         </div>
-    )
+        
+        </div>
+    </div>
+  );
 }
-
 export default DashboardChef
