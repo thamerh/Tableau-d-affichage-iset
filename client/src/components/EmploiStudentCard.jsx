ec72d022961ddeb87e1d9d3abf219ae3a38a0f3f
thamerh <hamdithamer99@gmail.com>
2022-08-12 00:00:33 +0100

    feat: add card for emploi student

---
+++ client/src/components/EmploiStudentCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Card,Button} from "react-bootstrap"
+import { Link } from 'react-router-dom'
+
+const EmploiCardStudent = ({ affiche }) => {
+    return (
+      
+        <>
+
+            <Card className='shadow-lg m-3 p-3 rounded' style={{ width: '25rem' }}>
+                <Card.Img src ={` http://localhost:5000/${affiche.image}` } style={{ height: '18rem' }}/>
+                <Card.Title>department: {affiche.department}</Card.Title>
+                <Card.Title>classe: {affiche.classe}</Card.Title>
+                <Card.Body> 
+                    <Link to={`EmploiDetailStudent/${affiche.id}`}>
+                        <Button variant="primary">Detail</Button>
+                    </Link>
+                </Card.Body>
+            </Card>   
+        </>
+ 
+ )
+}
+
+export default EmploiCardStudent;
