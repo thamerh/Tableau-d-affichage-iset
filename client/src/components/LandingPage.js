fca179b61bd732f54a37adc1f7fc797bc95bf199
thamerh <hamdithamer99@gmail.com>
2022-08-21 01:11:03 +0100

    style: fixed error in responsive style

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-import '../App.css'
-//import BackgroundImage from '../assets/images/bg.png'
+import './LandingPage.css';
+import { Footer } from './Footer';
 
 export  function LandingPage() {
     return (
-      <div style={ HeaderStyle }>
+      <>
+
         <header >
             <h2 className="main-title text-center">The university bulletin board is everywhere</h2>
             <p className="main-para text-center">join us now and don't waste time</p>
-        <div class="navbar-item has-dropdown is-hoverable " id="style-dropdown1">
-        <a class="navbar-link "  id='drop-don-style1'>
+    </header>
+    <div
+			className="d-flex flex-row justify-content-center w-80 mt-0 mb-0 "
+			style={{ width: "100%", margin: "0px"}}>
+			<div style={{ display: "flex", gap: "-4rem"  }}>
+        <span class="navbar-item has-dropdown is-hoverable " id="style-dropdown1" >
+        <a class="navbar-link "  id='drop-down-style1'>
         Log in
         </a>
         <div class="navbar-dropdown ">
@@ -26,9 +31,10 @@ export  function LandingPage() {
           </a>
         </div>
         
-  </div>
-  <div class="navbar-item has-dropdown is-hoverable" id="style-dropdown2">
-        <a class="navbar-link " id='drop-don-style2'>
+  </span>
+				
+  <span class="navbar-item has-dropdown is-hoverable " id="style-dropdown2">
+        <a class="navbar-link " id='drop-down-style2'>
         Sign Up
         </a>
         <div class="navbar-dropdown ">
@@ -42,35 +48,11 @@ export  function LandingPage() {
             Etudiant
           </a>
         </div>
-     </div>   
-    
-    </header>
+     </span> 
+			</div>
+		</div>
     <Footer />
-    </div>
+    </>
     )
 }
-const HeaderStyle = {
-    width: "100%",
-    height: "100vh",
-    background: `url("https://images.pexels.com/photos/633409/pexels-photo-633409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
-}
-export const Footer = () => {
-  return (
-      <p className="text-center" style={ FooterStyle }>Designed & coded by <span className="has-text-info">Thamer Hamdi</span></p>
-  )
-}
 
-const FooterStyle = {
-  background: "#fff",
-  fontSize: ".9rem",
-  color:"#222" ,
-  position: "absolute",
-  bottom: 0,
-  padding: "1rem",
-  margin: 0,
-  width: "100%",
-  opacity: ".6"
-}
