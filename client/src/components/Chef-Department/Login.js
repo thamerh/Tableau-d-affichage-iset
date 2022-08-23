51b62aef328668268133bb3b1098829f3401dbbd
thamerh <hamdithamer99@gmail.com>
2022-08-23 23:07:28 +0100

    style: fix background in login chef department

--- client/src/components/Chef-Department/Login.js
+++ client/src/components/Chef-Department/Login.js
@@ -26,7 +26,7 @@ export const LoginChef = () => {
     }
 
     return (
-        <div style={ HeaderStyle }>
+        <div >
         <section className=" is-fullheight is-fullwidth" >
             <div className="hero-body">
                 <div className="container">
@@ -67,11 +67,3 @@ export const LoginChef = () => {
     )
 }
 
-export const HeaderStyle = {
-    width: "100%",
-    height: "100vh",
-    background: `url("https://images.pexels.com/photos/633409/pexels-photo-633409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
-}
