a20a73dbb7ae331b54826a44c5c83996fac5dc0b
thamerh <hamdithamer99@gmail.com>
2022-08-28 23:18:53 +0100

    fix: fixed conflit in AddEmploi secreen

--- client/src/screens/Chef-dep/AddEmploi.jsx
+++ client/src/screens/Chef-dep/AddEmploi.jsx
@@ -145,14 +145,4 @@ const getNomDepByNameChef = async (name) => {
     )
 }
 export default AddEmploii;
-<<<<<<< HEAD
-const HeaderStyle = {
-    width: "100%",
-    height: "100vh",
-    background: `url("https://images.pexels.com/photos/633409/pexels-photo-633409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
-}
-=======
->>>>>>> dev1
+
